a982b8b3431d1c6d6725a8cee03d08dff9c0f24d
Saiful Shuvo <saif.shuvo@hotmail.com>
2021-12-27 12:43:02 +0600

    Update

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyBGoJ9Ceo2VwC4m1uPpbt9FrH65ZPyEmtY",
+    authDomain: "crimsons-it.firebaseapp.com",
+    projectId: "crimsons-it",
+    storageBucket: "crimsons-it.appspot.com",
+    messagingSenderId: "763860173133",
+    appId: "1:763860173133:web:ceb20e05416ceff7fe7704"
 };
 export default firebaseConfig;
 
